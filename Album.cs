de2a2f977c79202ae156fd9c06e6dff4f8c8e7a1
Lutiero <lutiero@gmail.com>
2023-07-20 07:44:36 -0400

    Estudo de classes

---
+++ Album.cs
@@ -0,0 +1,28 @@
+﻿class Album
+{
+    public Album(string nome)
+    {
+        Nome = nome;
+    }
+
+    private List<Musica> musicas = new List<Musica>();
+    public string Nome { get; }
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void MostraMusicas()
+    {
+        Console.WriteLine("************ Lista de músicas *******");
+        musicas.ForEach(musica => 
+        {
+            Console.WriteLine($"{musica.Nome}");
+        });
+        Console.WriteLine($"\nPara ouvir esse álbum inteiro você precisa de {DuracaoTotal} segundos.");
+    }
+
+
+}
---
+++ Musica.cs
@@ -0,0 +1,26 @@
+﻿class Musica
+{
+    public Musica(Banda artista, string nome)
+    {
+        Nome = nome;
+        Artista = artista;
+    }
+
+    public string? Nome { get; }
+    public Banda Artista { get; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+
+    public string? DescricaoResumida => $"\nA música '{Nome}' pertence a banda '{Artista.Nome}'.";
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine("************ Ficha Técnica *******");
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista.Nome}");
+        Console.WriteLine($"Duração: {Duracao}");
+        string disponibilidade = (Disponivel) ? "Sim" : "Não";
+        Console.WriteLine($"Disponibilidade: {disponibilidade}");
+        Console.WriteLine($"Descrição resumida: {DescricaoResumida}");
+    }
+}
